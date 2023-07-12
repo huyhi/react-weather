f9fbbafc0a8277d3ada83cac49b4a894aa214cc6
annhuny <annhuny@gmail.com>
2023-07-12 02:07:28 +0100

    map add flyTo transition

--- src/app/components/weather-map.js
+++ src/app/components/weather-map.js
@@ -1,18 +1,32 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { useSelector } from "react-redux"
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import { OPEN_WEATHER_KEY } from '../api'
+import { isEmptyObj } from '../common'
 import './weather-map.css'
 import 'leaflet/dist/leaflet.css';
 
+function ChangeView( {searchCity} ) {
+  const map = useMap()
+  if (!isEmptyObj(searchCity)) {
+    map.flyTo([searchCity.lat, searchCity.lon], 10)
+  }
+}
+
+
 export default function WeatherMap() {
-  
-  const position = [50.905, 6.500]
+
+  const defaultLondonCoord = [51.507, -0.125]
+  const defaultEuCoord = [51.592, 5.477]
+
+  const searchCityG = useSelector(state => state.searchCityG)
 
   return (
     <div className='map-container'>
       <div className='map-title'>
         Rain Forecast Radar
       </div>
-      <MapContainer id='map' center={position} zoom={4} scrollWheelZoom={false}>
+      <MapContainer id='map' center={ defaultEuCoord } zoom={4} scrollWheelZoom={false}>
+        <ChangeView searchCity={searchCityG} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
